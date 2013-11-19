04022e663c3417b9fd4959c0f6cde720d8182dc2
Christian Lapointe <clapointe@pyxis-tech.com>
2013-11-19 23:14:15 +0100

    json content type

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var port = 3700;
 var json_result = {"alive" : true };
 
 app.get("/ping" , function(req, res){
-     res.send(JSON.stringify(json_result));
+	res.writeHead(200, { 'Content Type': 'application/json'});
+     res.end(JSON.stringify(json_result));
      });
       
 app.listen(process.env.PORT || port);
