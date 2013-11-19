d961bf40f633aa5f766a6197699fe32f1bdac39c
Christian Lapointe <clapointe@pyxis-tech.com>
2013-11-19 23:27:29 +0100

    rollback on simplified json because it set the charset to utf-8. Not good for level 1.1 spec

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ var port = 3700;
 var json_result = {"alive" : true };
 
 app.get("/ping" , function(req, res){
-     res.json(JSON.stringify(json_result));
-     });
+	res.writeHead(200, { 'Content Type': 'application/json'});
+	res.end(JSON.stringify(json_result));
+	});
       
 app.listen(process.env.PORT || port);
 console.log("Listening on port " + process.env.PORT || port);
