3d50bf13cc6c7135168c973e703807431510d000
Christian Lapointe <clapointe@pyxis-tech.com>
2013-11-19 23:17:41 +0100

    simplified header processing using res.json

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ var port = 3700;
 var json_result = {"alive" : true };
 
 app.get("/ping" , function(req, res){
-	res.writeHead(200, { 'Content Type': 'application/json'});
-     res.end(JSON.stringify(json_result));
+     res.json(JSON.stringify(json_result));
      });
       
 app.listen(process.env.PORT || port);
