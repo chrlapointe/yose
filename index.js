b41d215645d67fd4540763b7a8560337b177fe3b
Christian Lapointe <clapointe@pyxis-tech.com>
2013-11-22 14:19:22 +0100

    reviving the get on /

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ var port = 3700;
  
 var json_result = {"alive" : true };
 
+app.get("/get" , function(req, res){
+	res.send("hello yose");
+	});
+      
 app.get("/ping" , function(req, res){
 	res.writeHead(200, { 'Content-Type': 'application/json'});
 	res.write(JSON.stringify(json_result));
 	res.end();
 	});
-      
 app.listen(process.env.PORT || port);
 console.log("Listening on port " + process.env.PORT || port);
