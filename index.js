fe83c68eff20367fdba91cb3c40d1ed333f61cab
Christian Lapointe <clapointe@pyxis-tech.com>
2013-11-23 01:02:42 +0100

    challenge 2.4. refactnt prime array up to sqrt(1e6)

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ var app = express();
 var port = 3700;
  
 var ping_result = {"alive" : true };
-
-function isNumber(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-}
+var primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541,547,557,563,569,571,577,587,593,599,601,607,613,617,619,631,641,643,647,653,659,661,673,677,683,691,701,709,719,727,733,739,743,751,757,761,769,773,787,797,809,811,821,823,827,829,839,853,857,859,863,877,881,883,887,907,911,919,929,937];
 
 function write_json(res, json) {
 	res.writeHead(200, { 'Content-Type': 'application/json'});
@@ -14,37 +11,25 @@ function write_json(res, json) {
 	res.end();
 }
 
-function getPrimes(max) {
-	var sieve = [], i, j, primes = [];
-	for (i = 2; i <= max; ++i) {
-		if (!sieve[i]) {
-			// i has not been marked -- it is prime
-			primes.push(i);
-			for (j = i << 1; j <= max; j += i) {
-				sieve[j] = true;
-			}
-		}
-	}
-	console.log("primes up to " + max + " are: " + primes);
-	return primes;
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
 function decompositions(number) {
-	var primes = getPrimes(Math.sqrt(number));
     var results = [];
-    var reste = number;
+	var maxPrime = Math.sqrt(number);
     var i = 0;
 
-    while (reste > 1 && i < primes.length) {
-        while ((reste % primes[i]) == 0) {
-            reste = reste / primes[i];
+    while (number > 1 && primes[i] <= maxPrime) {
+        while ((number % primes[i]) == 0) {
+            number = number / primes[i];
 			results.push(primes[i]);
         }
         i++;
     }
 
-    if (reste != 1) {
-        results.push(reste);
+    if (number != 1) {
+        results.push(number);
     }
 
     return results;
