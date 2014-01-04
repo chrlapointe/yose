07aa5ee91a53233ff0b7c11364956bde9d8eef1b
Christian Lapointe <clapointe@pyxis-tech.com>
2014-01-04 17:18:02 +0100

    corected ids for minesweeper cells

--- spec/minesweeper.spec.js
+++ spec/minesweeper.spec.js
@@ -15,11 +15,22 @@ describe('Passing the minesweeper (world 3) challenge', function() {
         testServer.close(); 
     });
    
-    it('answsers with a html page containing a minesweeper title and a grid named cell-nxp', function(done) {
+    it('answsers with a html page containing a minesweeper title', function(done) {
         zombie.visit('http://localhost:7000/minesweeper', function(error, browser, status) {
 			expect(browser.text("#title")).toEqual("Minesweeper");
-            expect(browser.querySelector("#cell-nxp")).not.toBeNull();
             done(); 
         });
     });
+	
+    it('answsers with a html page containing a grid named cell-nxp and cells with ids', function(done) {
+        zombie.visit('http://localhost:7000/minesweeper', function(error, browser, status) {
+            expect(browser.querySelector("#cell-nxp")).not.toBeNull();
+            expect(browser.querySelector("#cell-1x1")).not.toBeNull();
+            expect(browser.querySelector("#cell-1x8")).not.toBeNull();
+            expect(browser.querySelector("#cell-3x7")).not.toBeNull();
+            expect(browser.querySelector("#cell-8x8")).not.toBeNull();
+            done(); 
+        });
+    });
+	
 });
